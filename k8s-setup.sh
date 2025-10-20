799c3f8004efc54cc762d80218b9948e209b2466
Shubham <140893209+ssupshub@users.noreply.github.com>
2025-10-20 20:27:31 +0530

    Update k8s-setup.sh

--- k8s-setup.sh
+++ k8s-setup.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-# Update and install necessary packages
-apt-get update && apt-get upgrade -y
+# Start Docker (needed in some environments)
+service docker start
 
-# Install Docker (if not installed already)
-if ! command -v docker &> /dev/null
-then
-    apt-get install -y docker.io
-fi
+# Create kind cluster
+kind create cluster --name docker-k8s
 
-# Initialize Kubernetes using kubeadm
-kubeadm init --pod-network-cidr=10.244.0.0/16
+# Export kubeconfig
+kind get kubeconfig > /root/.kube/config
 
-# Set up kubeconfig
-export KUBEVIRT_VERSION=0.45.0
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-
-# Install a pod network add-on (Flannel)
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-
-# To allow the worker nodes to join the cluster, untaint the master node
-kubectl taint nodes --all node-role.kubernetes.io/master-
-
-# Kubernetes is now set up in Docker inside Ubuntu!
+echo "Kubernetes cluster is running inside Docker!"
+echo "Use kubectl to interact with it. Example:"
+echo "kubectl get nodes"
